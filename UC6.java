755de78417f0f1e0f0829b55a569bf82278f780d
amar1995k <67822357+amar1995k@users.noreply.github.com>
2021-07-08 19:55:24 +0530

    Add files via upload

---
+++ UC6.java
@@ -0,0 +1,11 @@
+package emp;
+
+public class UC6 {
+	public static void main(String[] args) {
+		System.out.println("Roll the die");
+		int x= (int)(Math.random()*10) % 6;
+		System.out.println("got number = "+x);
+	}
+	
+
+}
